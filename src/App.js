499437d433f4b0015cb49c422f5132d502bba546
Dania Lucas <76031192+danialucasm@users.noreply.github.com>
2021-06-22 17:27:53 -0300

    Proyecto final

--- src/App.js
+++ src/App.js
@@ -16,45 +16,52 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div id='Parrafo'> 
+          La Organización Mundial de la Salud estima que un peso saludable es el que se sitúa entre valores de 18,5 y 24,9.<br/>
+          Los valores inferiores a 18,5 nos indican un peso demasiado bajo.<br/>
+          Si son iguales o superiores a 25 se consideraría ya sobrepeso. <br/>
+          Si supera los 30 se trata de obesidad. <br/>
+        </div>
         <div>
-          <label style={{fontSize: 24, fontFamily :"cursive"}}>Peso (Ej: 60.8): </label>
+          <label style={{fontSize: 25, fontFamily :'serif'}}>Peso Actual: </label>
           <input  
             type='number'   
             min= '0'    
             value={numA}
-            style={{fontSize: 24, width: 100, textAlign: 'center'}}
+            style={{fontSize: 20, width: 90, textAlign: 'center'}}
             onChange={(e) => setNumA(+e.target.value)}
           />
-          <label style={{fontSize: 24, fontFamily :"cursive"}}>  Estatura(Ej: 1.60): </label>
+          <label style={{fontSize: 25, marginLeft: 30, fontFamily:'serif' }}>  Estatura: </label>
           <input
             type='number'
             min='0'
             value={numB}
-            style={{fontSize: 24, width: 100, textAlign: 'center'}}
+            style={{fontSize: 20, width: 90, textAlign: 'center'}}
             onChange={(e) => setNumB(+e.target.value)}
           />
-        </div>
-        <div>
           <button
-            style={{fontSize: 24, margin: '40px 0', padding: '3px', fontFamily :"cursive"}}
+            style={{fontSize: 20, margin: '30px 0', marginLeft: 35, padding: '3px', backgroundColor: 'white', fontFamily :'serif'}}
             onClick={calcularResultado}
           >
-            Calcular Peso
+             Calcular Peso 
           </button>
         </div>
         <div>
-          <label style={{fontSize: 24, fontFamily :"cursive"}}>Resultado: </label>
+          <label style={{fontSize: 25, fontFamily :'serif'}}>Resultado: </label>
           <input
             value={total}
-            style={{fontSize: 24, width: 100, textAlign: 'center', backgroundColor: 'white'}}
+            style={{fontSize: 20, width: 100, textAlign: 'center', backgroundColor: 'white'}}
             disabled
             readOnly
           />
         </div>
-        <h3>
+        <div>
+        <h3> <br/><br/><br/><br/>
           by Dania Lucas - ISI UTN FRRe <br /><br /><br />
         </h3>
+        </div>
       </header>
+      
     </div>
   );
 }
