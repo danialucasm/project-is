b584748e5810601851c966a7b9f387daf6aa4561
danialucasm <danulucas44@gmail.com>
2021-06-27 19:53:29 -0300

    Error de prueba arreglado

--- src/Operacion.test.js
+++ src/Operacion.test.js
@@ -1,7 +1,7 @@
 import { division } from './Operacion';
 
 test('division', () => {
-  expect(division(68.5, 1.70)).toBe((40).toFixed(2));
+  expect(division(68.5, 1.70)).toBe((23.70).toFixed(2));
   expect(division(71.4025, 1.69)).toBe((25.00).toFixed(2));
   expect(division(0, 1.70)).toBe((0.00).toFixed(2));
 });
